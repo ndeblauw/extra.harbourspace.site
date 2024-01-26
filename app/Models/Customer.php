2173910afe7d338fae51de04097fd3b419091d4e
Khalid M. Sheet <khalid.m.sheet@gmail.com>
2024-01-26 13:44:13 +0300

    REFACTOR: :hammer: return types and new relations for User and Customer models

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 
@@ -11,8 +12,13 @@ class Customer extends Model
 {
     use HasFactory, SoftDeletes;
 
-    public function users()
+    public function users(): HasMany
     {
         return $this->hasMany(User::class);
+    }
+
+    public function invoices(): HasMany
+    {
+        return $this->hasMany(Invoice::class);
     }
 }
