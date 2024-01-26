52d45442c362e06ec4466f7fd1894c86cd27e68d
Khalid M. Sheet <khalid.m.sheet@gmail.com>
2024-01-26 12:41:01 +0300

    FEATURE: :sparkles: add factories and seeders for user and customer models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(2)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            CustomerSeeder::class,
+            UserSeeder::class,
+        ]);
     }
 }
