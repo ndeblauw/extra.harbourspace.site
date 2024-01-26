5ca12583d0b4c3ed591bf635fd9bd0becd2672bb
Samuel David Suárez Rodríguez <42628316+samueldsr99@users.noreply.github.com>
2024-01-26 04:56:02 -0500

    FEATURE ✨ Add `Invoice`, `InvoiceLine` models, migrations & seeders (#3)
    * Add Invoice model, factory, and migration files
    
    * CONTENT :pencil: Update basic (default) info
    
    * FEATURE :sparkles: Add & install Breeze
    
    * FEATURE :sparkles: Add development tooling
    
    * UPDATE :package: Bump to latest versions of the packages
    
    * FEATURE :sparkles: Switch to CDN for Tailwind CSS during dev
    
    * FEATURE :sparkles: Add livewire
    
    * feat: Add partial `Invoice` model & migration
    
    * bugfix: remove `price` from `InvoiceLine`
    
    * refactor: make `product_id` nullable
    
    * feat: create `InvoiceLineFactory`
    
    * feat: add `Invoices` seeds
    
    * Add invoice_date to fillable fields in Invoice model
    
    * Fix invoice line relationship and price in Invoice model
    
    * Add foreign key for customer_id in invoices table
    
    * Add Invoice model, factory, and migration files
    
    * feat: Add partial `Invoice` model & migration
    
    * bugfix: remove `price` from `InvoiceLine`
    
    * refactor: make `product_id` nullable
    
    * feat: create `InvoiceLineFactory`
    
    * feat: add `Invoices` seeds
    
    * Add invoice_date to fillable fields in Invoice model
    
    ---------
    
    Co-authored-by: fvaldes0109 <fvaldes0109@gmail.com>
    Co-authored-by: Nico Deblauwe <nico@bluepundit.eu>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Invoice;
+use App\Models\InvoiceLine;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $invoices = Invoice::factory(10)->create();
+
+        $invoices->each(function ($invoice) {
+            InvoiceLine::factory(5)->create([
+                'invoice_id' => $invoice->id,
+            ]);
+        });
     }
 }
