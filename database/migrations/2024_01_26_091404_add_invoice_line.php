6424fbefbdcbf4617c446d9c0a8d908c50409468
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-01-26 11:49:24 +0100

    feat: add soft deletes to `InvoiceLine`

--- database/migrations/2024_01_26_091404_add_invoice_line.php
+++ database/migrations/2024_01_26_091404_add_invoice_line.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->foreignId('product_id')->nullable();
             $table->integer('quantity');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
