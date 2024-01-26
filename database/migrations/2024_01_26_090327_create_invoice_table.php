90868c23b2a09d1ab3cbc8ac998327d3346efe4b
fvaldes0109 <fvaldes0109@gmail.com>
2024-01-26 10:37:49 +0100

    Add foreign key for customer_id in invoices table

--- database/migrations/2024_01_26_090327_create_invoice_table.php
+++ database/migrations/2024_01_26_090327_create_invoice_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
             $table->date('invoice_date');
+            $table->foreignId('customer_id')->nullable();
             $table->timestamps();
         });
     }
