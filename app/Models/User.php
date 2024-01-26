ae3a845b6c27074906f8978899489448cdfb8557
Khalid M. Sheet <khalid.m.sheet@gmail.com>
2024-01-26 13:51:40 +0300

    REFACTOR: :hammer: Add return types and new relations for `User` and `Customer` (#8)
    * REFACTOR: :hammer: return types and new relations for User and Customer models
    
    * CHORE: :star: sorting relations alphabetically

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -44,7 +45,7 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    public function customer()
+    public function customer(): BelongsTo
     {
         return $this->belongsTo(Customer::class);
     }
