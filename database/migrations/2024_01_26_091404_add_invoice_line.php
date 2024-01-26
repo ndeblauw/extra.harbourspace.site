026016eb7f9656c1338e61558f8d7a8e5fd50c6c
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-01-26 10:17:02 +0100

    feat: Add partial `Invoice` model & migration

---
+++ database/migrations/2024_01_26_091404_add_invoice_line.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('invoice_lines', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('invoice_id')->references('id')->on('invoices')->onDelete('cascade');
+            $table->foreignId('product_id');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('invoice_lines');
+    }
+};
