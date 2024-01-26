26b86f8dc4d2911c3ffaef3b95bcd6045c7eec26
Brenda  Mukindia <mukindiabrenda67@gmail.com>
2024-01-26 12:20:15 +0100

    REFACTOR :hammer: Add Controller welcome page (#12)
    * Feature:Welcome page
    
    * tweaks:adding  style changes to the welcome app
    
    * tweaks:Changing  text for the welcome app
    
    * tweaks:adding a controller for welcome blade

---
+++ resources/views/website.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+<!
+        <!-- Scripts -->
+        <script src="https://cdn.tailwindcss.com"></script>
+        {{--@vite(['resources/css/app.css', 'resources/js/app.js'])--}}
+    </head>
+    <body class="antialiased">
+        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+            @if (Route::has('login'))
+                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+
+            <div class="p-6 text-center text-gray-800 dark:text-white">
+                <h1 class="text-4xl font-bold mb-4">Welcome to Invoicing App</h1>
+                <p class="text-lg">Start managing your invoices now!</p>
+            </div>
+        </div>
+        </body>
+        </html>
+
